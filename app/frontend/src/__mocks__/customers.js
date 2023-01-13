8e5fd0d65c01fb8474a657f21bf55c7aff0ab24f
saad.ali1272 <saad.ali1272@gmail.com>
2023-01-14 00:54:08 +0500

    Changes in frontend folder

---
+++ app/frontend/src/__mocks__/customers.js
@@ -0,0 +1,144 @@
+import { v4 as uuid } from 'uuid';
+
+export const customers = [
+  {
+    id: uuid(),
+    address: {
+      country: 'Pakistan',
+      state: 'Punjab',
+      city: 'Lahore',
+      street: 'Model Town'
+    },
+    avatarUrl: '/static/images/avatars/avatar_3.png',
+    createdAt: 1555016400000,
+    email: 'abdul.sami@ohex.io',
+    name: 'Abdul Sami',
+    phone: '0300-1234567'
+  },
+  {
+    id: uuid(),
+    address: {
+      country: 'Pakistan',
+      state: 'GB',
+      city: 'GB',
+      street: 'GB Road'
+    },
+    avatarUrl: '/static/images/avatars/avatar_4.png',
+    createdAt: 1555016400000,
+    email: 'abdul.hadi@ohex.io',
+    name: 'Abdul Hadi',
+    phone: '0301-1234567'
+  },
+  {
+    id: uuid(),
+    address: {
+      country: 'Pakistan',
+      state: 'Punjab',
+      city: 'Multan',
+      street: 'MLQ Road'
+    },
+    avatarUrl: '/static/images/avatars/avatar_2.png',
+    createdAt: 1555016400000,
+    email: 'nisar,haq@ohex.io',
+    name: 'Nisar Ul Haq',
+    phone: '0302-1234567'
+  },
+  {
+    id: uuid(),
+    address: {
+      country: 'Pakistan',
+      state: 'Sindh',
+      city: 'Karachi',
+      street: '3 Talwar'
+    },
+    avatarUrl: '/static/images/avatars/avatar_5.png',
+    createdAt: 1554930000000,
+    email: 'saad.umair@ohex.io',
+    name: 'Saad Umair',
+    phone: '0303-1234567'
+  },
+  {
+    id: uuid(),
+    address: {
+      country: 'Pakistan',
+      state: 'Sindh',
+      city: 'Sukkur',
+      street: 'Nagin Chorangi'
+    },
+    avatarUrl: '/static/images/avatars/avatar_6.png',
+    createdAt: 1554757200000,
+    email: 'km.hassan@ohex.io',
+    name: 'M. Hassan',
+    phone: '0305-1234567'
+  },
+  {
+    id: uuid(),
+    address: {
+      country: 'Pakistan',
+      state: 'Punjab',
+      city: 'Lahore',
+      street: 'Milad Street'
+    },
+    avatarUrl: '/static/images/avatars/avatar_1.png',
+    createdAt: 1554670800000,
+    email: 'shahmeer.chappal@ohex.io',
+    name: 'Shahmeer Ali',
+    phone: '0300-1234577'
+  },
+  {
+    id: uuid(),
+    address: {
+      country: 'Pakistan',
+      state: 'KPK',
+      city: 'Peshawar',
+      street: 'Kati Pahari'
+    },
+    avatarUrl: '/static/images/avatars/avatar_7.png',
+    createdAt: 1554325200000,
+    email: 'irfan.mohd@ohex.io',
+    name: 'M. Irfan',
+    phone: '0307-1234567'
+  },
+  {
+    id: uuid(),
+    address: {
+      country: 'Pakistan',
+      state: 'Sindh',
+      city: 'Larkana',
+      street: 'Kotha Pind'
+    },
+    avatarUrl: '/static/images/avatars/avatar_8.png',
+    createdAt: 1523048400000,
+    email: 'kabeer.khan@ohex.io',
+    name: 'M. Kamil',
+    phone: '0308-1234567'
+  },
+  {
+    id: uuid(),
+    address: {
+      country: 'Pakistan',
+      state: 'AJK',
+      city: 'Mirpur',
+      street: 'F-12 Sector 2'
+    },
+    avatarUrl: '/static/images/avatars/avatar_9.png',
+    createdAt: 1554702800000,
+    email: 'hameed.ch@ohex.io',
+    name: 'M. Hameed',
+    phone: '0312-1234567'
+  },
+  {
+    id: uuid(),
+    address: {
+      country: 'Pakistan',
+      state: 'Balouchistan',
+      city: 'Quetta',
+      street: 'Khatarnak Road'
+    },
+    avatarUrl: '/static/images/avatars/avatar_10.png',
+    createdAt: 1522702800000,
+    email: 'Cap.quetta@ohex.io',
+    name: 'Aizaz Khan',
+    phone: '0309-1234567'
+  }
+];
---
+++ app/frontend/src/components/customer/customer-list-toolbar.js
@@ -0,0 +1,68 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  InputAdornment,
+  SvgIcon, Typography
+} from '@mui/material';
+import { Search as SearchIcon } from '../../icons/search';
+import { Upload as UploadIcon } from '../../icons/upload';
+import { Download as DownloadIcon } from '../../icons/download';
+
+export const CustomerListToolbar = (props) => (
+  <Box {...props}>
+    <Box
+      sx={{
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+        m: -1
+      }}
+    >
+      <Typography
+        sx={{ m: 1 }}
+        variant="h4"
+      >
+        Engineers
+      </Typography>
+      <Box sx={{ m: 1 }}>
+       
+        
+        <Button
+          color="primary"
+          variant="contained"
+        >
+          Add Team Members
+        </Button>
+      </Box>
+    </Box>
+    <Box sx={{ mt: 3 }}>
+      <Card>
+        <CardContent>
+          <Box sx={{ maxWidth: 500 }}>
+            <TextField
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SvgIcon
+                      color="action"
+                      fontSize="small"
+                    >
+                      <SearchIcon />
+                    </SvgIcon>
+                  </InputAdornment>
+                )
+              }}
+              placeholder="Search Team"
+              variant="outlined"
+            />
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
+  </Box>
+);
